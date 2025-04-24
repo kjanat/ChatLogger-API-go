5f40f671115167b2cd0cf82b9a468b5dc4b34dcd
Kaj Kowalski <dev@kajkowalski.nl>
2025-04-24 17:05:58 +0200

    Updates version to 0.1.10 and fixes artifact upload
    Updates the application version to 0.1.10.
    
    Removes the `github.run_id` from the artifact name to prevent issues with long artifact names.
    
    Passes the repository name to the release workflow to allow for more flexibility in image naming.

--- internal/version/version.go
+++ internal/version/version.go
@@ -5,7 +5,7 @@ package version
 // Version information.
 var (
 	// Version is the current version of the application.
-	Version = "0.1.9"
+	Version = "0.1.10"
 
 	// BuildTime is the time the binary was built.
 	BuildTime = "unknown"
