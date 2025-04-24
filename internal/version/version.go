ee737c7f71ecfa1312ff548739f51549dc11c8ca
Kaj Kowalski <dev@kajkowalski.nl>
2025-04-24 16:23:27 +0200

    Updates build workflow for release triggering
    Refactors the build workflow to trigger the release workflow
    based on the build job's outputs instead of the tag-version job.
    
    Also, updates the version to 0.1.7 and adds descriptions to the release workflow inputs.

--- internal/version/version.go
+++ internal/version/version.go
@@ -5,7 +5,7 @@ package version
 // Version information.
 var (
 	// Version is the current version of the application.
-	Version = "0.1.6"
+	Version = "0.1.7"
 
 	// BuildTime is the time the binary was built.
 	BuildTime = "unknown"
